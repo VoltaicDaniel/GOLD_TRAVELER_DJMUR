91296401ae527aff53f02b888c922d6799c7184a
JulianMuriel <49957063+JulianMuriel@users.noreply.github.com>
2019-05-18 18:36:01 -0500

    Add files via upload

---
+++ map/conejo_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="conejo_tileset" tilewidth="16" tileheight="16" spacing="2" tilecount="810" columns="27">
+ <image source="../conejo_tileset.png" width="485" height="546"/>
+</tileset>
